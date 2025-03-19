eee8036bd9921078dac3fb44f210b6380d0f1a83
4lokym <mykola.koshilko@gmail.com>
2025-03-19 19:42:34 +0100

    Make the base logic for tictactoe

---
+++ javascript.js
@@ -0,0 +1,86 @@
+const myBoard = function (){
+
+    let board;
+    clearBoard();
+
+
+    function emptyBoard_p(){
+        return [
+            ["~","~","~"],
+            ["~","~","~"],
+            ["~","~","~"]
+        ]
+    }
+
+    function clearBoard(){
+        board = emptyBoard_p();
+    }
+
+    function getBoard(){
+        return board;
+    }
+
+    function setSimbolOnBoard_p(x, y, is_X){
+        board[x%3][y%3] = is_X ? "X":"O";
+    }
+
+    function displayBoard(){
+        console.log(`${board[0][0]}|${board[0][1]}|${board[0][2]}`);
+        console.log(`${board[1][0]}|${board[1][1]}|${board[1][2]}`);
+        console.log(`${board[2][0]}|${board[2][1]}|${board[2][2]}`);
+    }
+
+    function displayWin_p(is_X = true){
+        console.log(`${is_X? "Player X":"Player O"} wins`);
+    }
+    function displayTie_p(){
+        console.log("It's a tie");
+    }
+
+    const check_p = function(){
+        let isTris = false;
+        for(let i = 0; i<board.length; i++){
+            isTris = (board[i][0] === board[i][1] && board[i][0] === board[i][2]);
+        }
+        
+        function columns(){
+            let isColumnTris = false;
+            for(let i = 0; i<board[0].length && !isColumnTris; i++){
+                isColumnTris = (board[0][i] !== "~" && board[0][i] === board[1][i] && board[0][i] === board[2][i]);
+            }
+            return isColumnTris;
+        }
+
+        function rows(){
+            let isRowTris = false;
+            for(let i = 0; i<board.length && !isRowTris; i++){
+                isRowTris = (board[i][0] !== "~" && board[i][0] === board[i][1] && board[i][0] === board[i][2]);
+            }
+            return isRowTris;
+        }
+
+        function obliques(){
+            let isObliqueTris = false;
+            return (((board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
+            (board[0][2] === board[1][1] && board[1][1] === board[2][0])) && board[1][1] !== "~");
+        }
+
+        function all(){
+            return (rows() || columns() || obliques());
+        }
+
+        return {
+            columns, rows, obliques, all
+        }
+    }();
+
+    function playHand(x, y, is_X){
+        setSimbolOnBoard_p(x, y, is_X);
+        displayBoard();
+        const win = check_p.all();
+        return {is_X, win};
+    }
+
+    return {getBoard, clearBoard, displayBoard, playHand};
+
+}();
